5b0e6e271716eace3cff96164ec1f2c44ad4f439
jonesj5019 <61749213+jonesj5019@users.noreply.github.com>
2020-03-03 11:11:53 -0500

    Basic math

---
+++ P2HW1_BasicMath_JamesJones.py
@@ -0,0 +1,16 @@
+    # Add and multiply two numbers
+    # 2/20/2020
+    # CTI-110 P2HW1 - Basic Math
+    # James Jones
+
+#Get the two numbers
+num1 = float(input('Enter your first number: '))
+num2 = float(input('Enter your second number: '))
+#Add and multiply the two numbers
+sum = num1+num2
+result = num1*num2
+#Display the numbers in a viewer friendly way
+print ('Your first number is ',num1)
+print ('Your second number is ',num2)
+print ('The sum of these two numbers is ',sum)
+print ('The result of them multiplied is ',result)
