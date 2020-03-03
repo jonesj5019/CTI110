cf1de4f4d220b8994f7f4b8ba2caadb6473859a1
jonesj5019 <61749213+jonesj5019@users.noreply.github.com>
2020-03-03 11:10:51 -0500

    Sales prediction

---
+++ P2T1_SalesPrediction_JonesJames.py
@@ -0,0 +1,11 @@
+# Get the projected total sales.
+# 2/13/2020
+# CTI-110 P2T1 - Sales Prediction
+# James Jones
+total_sales = float(input('Enter the projected sales:  '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ('The profit is $', format(profit, ',.2f'))
